8bc49029ac732def245e1f9978c31ec690eb4785
Dron <dron@dron>
2024-09-05 15:59:42 +0000

    fixes

--- src/drone_aruco/drone_aruco/cord_publisher.py
+++ src/drone_aruco/drone_aruco/cord_publisher.py
@@ -4,7 +4,8 @@ from rclpy.executors import ExternalShutdownException
 from rclpy.node import Node
 import rclpy.time
 from aruco_opencv_msgs.msg import ArucoDetection, MarkerPose
-from tf_transformations import euler_from_quaternion
+from tf_transformations import euler_from_quaternion, quaternion_matrix
+from drone_interfaces.msg import MarkerCords
 
 
 TAG_DICT = {
@@ -12,7 +13,6 @@ TAG_DICT = {
     102: "B",
     103: "C",
     104: "D",
-
 }
 
 class cord_publisher(Node):
@@ -20,38 +20,22 @@ class cord_publisher(Node):
         super().__init__("cord_publisher")
 
         self.marker_subscription = self.create_subscription(ArucoDetection, "/aruco_detections", self.marker_pose_callback, 10)
-        self.marker_publisher = self.create_publisher(MarkerPose, "marker_cords",10)
+        self.marker_publisher = self.create_publisher(MarkerCords, "/marker_cords",10)
 
 
     def marker_pose_callback(self, msg: ArucoDetection):
-        marker_cords = MarkerPose()
+        marker_cords = MarkerCords()
         markers:  list[MarkerPose] = msg.markers
 
         for marker in markers:
+            if marker.marker_id not in TAG_DICT:
+                continue
             marker_cords.marker_id = TAG_DICT[marker.marker_id]
-            marker_cords.pose.position.x = marker.pose.position.x
-            marker_cords.pose.position.y = marker.pose.position.y
+            
+            marker_cords.x_pos = marker.pose.position.x
+            marker_cords.y_pos = marker.pose.position.y
 
             self.marker_publisher.publish(marker_cords)
-
-    def filter_markers(self, msg: MarkerPose):
-        orientation = [
-            msg.pose.orientation.x, 
-            msg.pose.orientation.y, 
-            msg.pose.orientation.z, 
-            msg.pose.orientation.w
-        ]
-        (roll, pitch, yaw) = euler_from_quaternion(orientation)
-
-        self.pose_roll = roll
-        self.pose_pitch = pitch
-        self.pose_yaw = yaw
-
-        print(roll, pitch, yaw)
-        
-        
-
-
             
 
 def main():
